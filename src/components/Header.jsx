e3ee930d49aaf045319f3e4a8f325c55147652ce
MarlonChi <marlonchiodelli@hotmail.com>
2023-07-26 20:28:45 -0300

    feat: instadogs init

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../assets/dogs.svg";
+
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" aria-label="Dogs - Home" className={styles.logo}>
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
