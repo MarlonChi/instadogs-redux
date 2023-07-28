c57cb51d22efa734a3caa9b726b7cbad4a8525a2
MarlonChi <marlonchiodelli@hotmail.com>
2023-07-28 17:47:35 -0300

    feat: photo modal

--- src/components/Feed/FeedModal.jsx
+++ src/components/Feed/FeedModal.jsx
@@ -1,5 +1,34 @@
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+import { useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
+import { PHOTO_GET } from "../../api";
+
+import PhotoContent from "../Photo/PhotoContent";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+
+import styles from "./FeedModal.module.css";
+
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  const handleOutsideClick = (event) => {
+    if (event.target === event.currentTarget) {
+      setModalPhoto(null);
+    }
+  };
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
---
+++ src/components/Photo/PhotoContent.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+import styles from "./PhotoContent.module.css";
+import PhotoComments from "./PhotoComments";
+
+const PhotoContent = ({ data }) => {
+  const { photo, comments } = data;
+
+  const dogAge = () => {
+    if (photo.idade == 1) {
+      return "1 ano";
+    } else {
+      return photo.idade + " anos";
+    }
+  };
+
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.views}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{dogAge()}</li>
+          </ul>
+        </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
+  );
+};
+
+export default PhotoContent;
