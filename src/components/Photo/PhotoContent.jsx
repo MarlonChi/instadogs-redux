809bbe69830223a8ae6e91d87fe5c94ecf4a43df
MarlonChi <marlonchiodelli@hotmail.com>
2023-07-29 10:45:55 -0300

    feat: skeleton

--- src/components/Photo/PhotoContent.jsx
+++ src/components/Photo/PhotoContent.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 
 import PhotoComments from "./PhotoComments";
+import PhotoDelete from "./PhotoDelete";
+import Image from "../Helper/Image";
 
 import styles from "./PhotoContent.module.css";
-import PhotoDelete from "./PhotoDelete";
 
 const PhotoContent = ({ data }) => {
   const user = useContext(UserContext);
@@ -22,7 +23,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
